e57d5df477cae496bbf213f692cb5ad408a7b1ee
Shivam <shivamnegi478@gmail.com>
2023-05-23 13:24:50 +0530

    Added booking model along with utils from flight service

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const { ServerConfig } = require('./config')
+
+const apiRoutes = require('./routes')
+
+const app = express()
+
+app.use('/api', apiRoutes);
+
+app.listen(ServerConfig.PORT, () => {
+    console.log(`successfully started the server on PORT : ${ServerConfig.PORT}`)
+})
+//console.log(process.env)
