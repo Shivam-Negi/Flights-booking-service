50e6736b1ee82ff3466d198b2e47826ebedc534c
Shivam <shivamnegi478@gmail.com>
2023-06-04 22:48:15 +0530

    started booking implementation

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ const { ServerConfig } = require('./config')
 
 const apiRoutes = require('./routes')
 
-const app = express()
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.use('/api', apiRoutes);
 
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -4,6 +4,10 @@ const router = express.Router()
 
 const { InfoController } = require('../../controllers')
 
-router.get('/info', InfoController.info)
+const bookingRoutes = require('./booking');
+
+router.get('/info', InfoController.info);
+
+router.use('/bookings', bookingRoutes)
 
 module.exports = router;
